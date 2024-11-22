24f19f1460e51c566840b10ff6bbe0261c46a84c
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-11-22 10:41:24 +0400

    changing font of description text to sans-serif and small styling changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import Logo from "../assets/logo.png";
 function Footer() {
   return (
     <div className="footer__container">
-      <Link><img className="logo" width="150px" height="64px" src={Logo} alt="Logo" /></Link>
+      <Link to="/"><img className="logo" width="150px" height="64px" src={Logo} alt="Logo" /></Link>
       <div>
         <h3>Saxo Jazz Djs</h3>
         <p>phone: +48 777 555 666</p>
